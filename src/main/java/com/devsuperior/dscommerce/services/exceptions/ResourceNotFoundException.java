318cb89340d44fc2bd1f91747aa24e4ed8b47122
André Fernandes Dias <andredias34@hotmail.com>
2024-04-21 12:04:53 -0300

    Criando exceções de serviço customizadas

---
+++ src/main/java/com/devsuperior/dscommerce/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,40 @@
+package com.devsuperior.dscommerce.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String msg){
+        super(msg);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
